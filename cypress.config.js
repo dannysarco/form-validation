a40b3155408156f24064d82393c98e1b6e15fd26
Danny Sarco <dsarco@gmail.com>
2024-02-14 09:32:46 -0500

    Add e2e and component testing configurations

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:8080",
+    specPattern: "cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    supportFile: "cypress/support/index.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    // any additional configurations
   },
+  component: {
+    // Component testing specific configurations (if used)
+  },
+  // Other global configurations can be added here
 });
